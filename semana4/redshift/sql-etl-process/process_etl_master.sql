d1ecde332afe78a60fd1a11263bfd8fb823f7547
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-05-13 09:28:50 +0000

    semana 4 primera parte

--- semana4/redshift/sql-etl-process/process_etl_master.sql
+++ semana4/redshift/sql-etl-process/process_etl_master.sql
@@ -1,6 +1,7 @@
--- \c etl_db ;
+\c etl_db ;
+DROP SCHEMA IF EXISTS desastres_final;
 CREATE SCHEMA desastres_final;
-
+DROP SCHEMA IF EXISTS desastres_bde;
 CREATE SCHEMA desastres_bde;
 
 CREATE TABLE
@@ -10,9 +11,8 @@ CREATE TABLE
         oxigeno NUMERIC NOT NULL
     );
 
--- Insertar valores manualmente
 INSERT INTO
-    clima
+    desastres_final.clima
 VALUES
     (2023, 22.5, 230),
     (2024, 22.7, 228.6),
@@ -23,127 +23,62 @@ VALUES
     (2029, 23.6, 226.1),
     (2030, 23.8, 225.1);
 
-create table
+CREATE TABLE
     desastres_final.desastres (
-        year int not null primary key,
-        tsunamis int not null,
-        olas_calor int not null,
-        terremotos int not null,
-        erupciones int not null,
-        incendios int not null
+        year INT NOT NULL PRIMARY key,
+        tsunamis INT NOT NULL,
+        olas_calor INT NOT NULL,
+        terremotos INT NOT NULL,
+        erupciones INT NOT NULL,
+        incendios INT NOT NULL
     );
 
-
 INSERT INTO
-    desastres
+    desastres_final.desastres
 VALUES
-    (2023, 2, 15, 6, 7, 50);
-
-INSERT INTO
-    desastres
-VALUES
-    (2024, 1, 12, 8, 9, 46);
-
-INSERT INTO
-    desastres
-VALUES
-    (2025, 3, 16, 5, 6, 47);
-
-INSERT INTO
-    desastres
-VALUES
-    (2026, 4, 12, 10, 13, 52);
-
-INSERT INTO
-    desastres
-VALUES
-    (2027, 5, 12, 6, 5, 41);
-
-INSERT INTO
-    desastres
-VALUES
-    (2028, 4, 18, 3, 2, 39);
-
-INSERT INTO
-    desastres
-VALUES
-    (2029, 2, 19, 5, 6, 49);
-
-INSERT INTO
-    desastres
-VALUES
+    (2023, 2, 15, 6, 7, 50),
+    (2024, 1, 12, 8, 9, 46),
+    (2025, 3, 16, 5, 6, 47),
+    (2026, 4, 12, 10, 13, 52),
+    (2027, 5, 12, 6, 5, 41),
+    (2028, 4, 18, 3, 2, 39),
+    (2029, 2, 19, 5, 6, 49),
     (2030, 4, 20, 6, 7, 50);
 
--- 4. crear tabla muertes proyectadas por rangos de edad
 CREATE TABLE
-    muertes (
-        año INT NOT NULL PRIMARY KEY,
-        R_Menor15 INT NOT NULL,
-        R_15_a_30 INT NOT NULL,
-        R_30_a_45 INT NOT NULL,
-        R_45_a_60 INT NOT NULL,
-        R_M_a_60 INT NOT NULL
+    desastres_final.muertes (
+        year INT NOT NULL PRIMARY key,
+        r_menor15 INT NOT NULL,
+        r_15_a_30 INT NOT NULL,
+        r_30_a_45 INT NOT NULL,
+        r_45_a_60 INT NOT NULL,
+        r_m_a_60 INT NOT NULL
     );
 
-GO
--- Insertar valores manualmente
 INSERT INTO
-    muertes
+    desastres_final.muertes
 VALUES
-    (2023, 1000, 1300, 1200, 1150, 1500);
-
-INSERT INTO
-    muertes
-VALUES
-    (2024, 1200, 1250, 1260, 1678, 1940);
-
-INSERT INTO
-    muertes
-VALUES
-    (2025, 987, 1130, 1160, 1245, 1200);
-
-INSERT INTO
-    muertes
-VALUES
-    (2026, 1560, 1578, 1856, 1988, 1245);
-
-INSERT INTO
-    muertes
-VALUES
-    (2027, 1002, 943, 1345, 1232, 986);
-
-INSERT INTO
-    muertes
-VALUES
-    (2028, 957, 987, 1856, 1567, 1756);
-
-INSERT INTO
-    muertes
-VALUES
-    (2029, 1285, 1376, 1465, 1432, 1236);
-
-INSERT INTO
-    muertes
-VALUES
+    (2023, 1000, 1300, 1200, 1150, 1500),
+    (2024, 1200, 1250, 1260, 1678, 1940),
+    (2025, 987, 1130, 1160, 1245, 1200),
+    (2026, 1560, 1578, 1856, 1988, 1245),
+    (2027, 1002, 943, 1345, 1232, 986),
+    (2028, 957, 987, 1856, 1567, 1756),
+    (2029, 1285, 1376, 1465, 1432, 1236),
     (2030, 1145, 1456, 1345, 1654, 1877);
 
--- 5. Crear base de datos para alojar resumenes de estadisticas
-CREATE DATABASE DESASTRES_BDE;
-
-GO USE DESASTRES_BDE GO
+--< schema final>--
 CREATE TABLE
-    DESASTRES_FINAL (
-        Cuatrenio varchar(20) NOT NULL PRIMARY KEY,
-        Temp_AVG FLOAT NOT NULL,
-        Oxi_AVG FLOAT NOT NULL,
-        T_Tsunamis INT NOT NULL,
-        T_OlasCalor INT NOT NULL,
-        T_Terremotos INT NOT NULL,
-        T_Erupciones INT NOT NULL,
-        T_Incendios INT NOT NULL,
-        M_Jovenes_AVG FLOAT NOT NULL,
-        M_Adutos_AVG FLOAT NOT NULL,
-        M_Ancianos_AVG FLOAT NOT NULL
+    desastres_bde.desastres_final (
+        cuatrenio VARCHAR(20) NOT NULL PRIMARY KEY,
+        temp_avg NUMERIC NOT NULL,
+        oxi_avg NUMERIC NOT NULL,
+        t_tsunamis INT NOT NULL,
+        t_olascalor INT NOT NULL,
+        t_terremotos INT NOT NULL,
+        t_erupciones INT NOT NULL,
+        t_incendios INT NOT NULL,
+        m_jovenes_avg NUMERIC NOT NULL,
+        m_adutos_avg NUMERIC NOT NULL,
+        m_ancianos_avg NUMERIC NOT NULL
     );
-
-GO
